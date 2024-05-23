ab3e6d9efdd463f09acb2a0434c52f015eb1aa78
mauricio.cuello <mauricio.cuello@coderhouse.com>
2024-05-23 12:05:34 -0300

    usamos templates de internet

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -10,3 +10,17 @@ class Estudiante(models.Model):
 
   nombre = models.CharField(max_length=30)
   apellido = models.CharField(max_length=30)
+  email = models.EmailField(null=True)
+
+class Profesor(models.Model):
+
+  nombre = models.CharField(max_length=30)
+  apellido = models.CharField(max_length=30)
+  email = models.EmailField()
+  profesion = models.CharField(max_length=30)
+
+class Entregable(models.Model):
+
+  nombre = models.CharField(max_length=30)
+  fechaDeEntrega = models.DateField()
+  entregado = models.BooleanField()

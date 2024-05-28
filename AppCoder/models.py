2f09850102ebbc092b767f378879c3f4d8e72a46
mauricio.cuello <mauricio.cuello@coderhouse.com>
2024-05-27 22:24:17 -0300

    personalizamos el admin

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -6,11 +6,24 @@ class Curso(models.Model):
   nombre = models.CharField(max_length=40)
   camada = models.IntegerField()
 
+  def __str__(self):
+    return f'{self.nombre} - {self.camada}'
+  
+  class Meta():
+
+    verbose_name = 'Course'
+    verbose_name_plural = 'The courses'
+    ordering = ('nombre', 'camada')
+    unique_together = ('nombre', 'camada')
+
 class Estudiante(models.Model):
 
   nombre = models.CharField(max_length=30)
   apellido = models.CharField(max_length=30)
   email = models.EmailField(null=True)
+
+  def __str__(self):
+    return f'{self.nombre} - {self.apellido}'
 
 class Profesor(models.Model):
 
@@ -18,9 +31,11 @@ class Profesor(models.Model):
   apellido = models.CharField(max_length=30)
   email = models.EmailField()
   profesion = models.CharField(max_length=30)
+  cursos = models.ManyToManyField(Curso)
 
 class Entregable(models.Model):
 
   nombre = models.CharField(max_length=30)
   fechaDeEntrega = models.DateField()
   entregado = models.BooleanField()
+  estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
